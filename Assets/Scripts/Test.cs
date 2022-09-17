7b8a70a7c9499777426a23b1640eaf28d67cc858
Udara <uartcreations@gmail.com>
2022-09-17 23:34:11 +0530

    Finished XMLHandler class

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -10,13 +10,57 @@ public class Test : MonoBehaviour
    
     void Start()
     {
+
+        //Creating a new XML file and Writing
+        /*
+        _xmlHandler = new XMLHandler();
+        
+        //Create XML child elements
+        XmlElement myElement = _xmlHandler.CreateXMLElement("Save", new Vector3(5.26f, 5.68f, 8.12f));
+        XmlElement myElement2 = _xmlHandler.CreateXMLElement("Save", new Vector3(7.26f, 1.68f, 0.12f));
+
+        //Create and append XML root element
+        _xmlHandler.CreateRootElement("MyRoot");
+
+        //Append xml elements to the root element
+        _xmlHandler.AppendElementToRoot(myElement);
+        _xmlHandler.AppendElementToRoot(myElement2);
+
+        //write xml data to a xml file
+        _xmlHandler.WriteXMLDocument("Hello");
+        */
+
+
+        //Open Existing XML file and Reading
+        
         _xmlHandler = new XMLHandler();
 
-        XmlElement myElement = _xmlHandler.CreateXMLElement("Save", 10);
+        //Open an existing XML file
+        _xmlHandler.OpenXMLDocument("Hello");
 
-        _xmlHandler.CreateRootElement("MyRoot");
-        _xmlHandler.AppendElementToRoot(myElement);
-        _xmlHandler.WriteXMLDocument("Hello");
+        //Get data of all the child elements in the xml file
+        List<string> dataList = _xmlHandler.GetXMLRawData();
+
+        //Create List in Vector3 type
+        List<Vector3> myDataList = new List<Vector3>();
+
+        //If XML Raw string data is in vector3, then desirealize those data back into a Vector3 format
+        // using "GetVector3Data" method in the XMLHandler class
+        foreach (string res in dataList)
+        {
+            //Deserialize Vector3 string data back to the Vector3 data
+            myDataList.Add(_xmlHandler.GetVector3Data(res));
+        }
+
+        //Read those deserialized Vector3 data
+        foreach(Vector3 d in myDataList)
+        {
+            Debug.Log(d);
+        }
+        
+        
+   
+
     }
 
    
